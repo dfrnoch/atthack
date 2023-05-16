4d588b0b4d1c2a957ad97ca0d8ea9ba004f2f389
lnxcz <frnoch@pm.me>
2023-05-16 21:19:54 +0200

    feat: :sparkles: invite info

--- src/pages/invite/[code].tsx
+++ src/pages/invite/[code].tsx
@@ -1,22 +1,26 @@
 import { Button, Center, Container, Space, Title } from "@mantine/core";
 import { signIn, useSession } from "next-auth/react";
+import { api } from "~/utils/api";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const InvitePage = () => {
   const session = useSession();
+  const router = useRouter();
 
+  const inviteData = api.invite.getInviteInfo.useQuery(router.query.code as string);
 
-  //
   return (
     <Container className="h-screen">
       <Center className="h-full">
         <div className="flex flex-col items-center gap-6">
           <Title className="text-2xl">Pozvanka</Title>
 
-          {session.status !== "authenticated" && (
+          {(session.status !== "authenticated" && (
             <Button variant="filled" onClick={() => signIn()}>
               Přihlásit se
             </Button>
-          )}
+          )) || <div className="w-full">cau p</div>}
         </div>
       </Center>
     </Container>
