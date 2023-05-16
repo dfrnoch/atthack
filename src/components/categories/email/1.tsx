e3f154966d273ef67a5765cb784eeecfb2dba437
Matěj Pešl <matejpesl1@gmail.com>
2023-05-16 13:50:20 +0200

    reformat

--- src/components/categories/email/1.tsx
+++ src/components/categories/email/1.tsx
@@ -1,14 +1,14 @@
 import {Title} from "@mantine/core";
 
 const EmailExercise1 = () => {
-  return (
-    <div>
-      <Title>
-          Spamovací email atd.
-      </Title>
-        <p>
-            Nejaky popis idk
-        </p>
-    </div>
-  );
+    return (
+        <div>
+            <Title>
+                Spamovací email atd.
+            </Title>
+            <p>
+                Nejaky popis idk
+            </p>
+        </div>
+    );
 };
