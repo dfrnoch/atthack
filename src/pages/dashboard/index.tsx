4f7f902ee4f974c9156a72d2efc3b32bde2c0a36
lnxcz <frnoch@pm.me>
2023-05-16 22:57:25 +0200

    feat: :sparkles: soem changes

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,5 +1,11 @@
 import { Container, TextInput } from "@mantine/core";
 
+// import dynamic from "next/dynamic";
+
+// // Lazy load the components
+// const UserDashboard = dynamic(() => import("../components/UserDashboard"));
+// const AdminDashboard = dynamic(() => import("../components/AdminDashboard"));
+
 const Dashboard = () => {
   return (
     <Container>
