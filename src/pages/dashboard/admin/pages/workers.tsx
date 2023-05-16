ec5fa12cfbfbadeff42d83c3c7f91612d1c8dbb2
Atsukoro1 <dornicakkuba@gmail.com>
2023-05-16 21:35:47 +0200

    feat: 🎸 Workers

--- src/pages/dashboard/admin/pages/workers.tsx
+++ src/pages/dashboard/admin/pages/workers.tsx
@@ -3,63 +3,93 @@ import WorkerGroupCard from "../../../../components/adminDashboard/WorkerGroupCa
 import { useDisclosure } from "@mantine/hooks";
 import { useState } from "react";
 import { api } from "~/utils/api";
+import { CompanyWorkerGroup } from "@prisma/client";
 
 const WorkersPage = () => {
   const [modalOpened, { open, close }] = useDisclosure();
+  const workers = api.company.fetchWorkerGroups.useQuery();
+  const [selectedGroup, setSelectedGroup] = useState<CompanyWorkerGroup | null>(null);
   const createGroup = api.company.addWorkerGroup.useMutation({
-    onSuccess: () => {
-        close();
+    onSuccess: async () => {
+      close();
+      await workers.refetch();
+    },
+  });
+  const deleteWorkerGroup = api.company.removeWorkerGroup.useMutation({
+    onSuccess: async () => {
+        setSelectedGroup(null);
+        await workers.refetch();
     }
   });
   const [createGroupData, setCreateGroupData] = useState({
     name: "",
-    color: ""
+    color: "",
   });
 
   return (
     <div>
-      <Flex gap={15} direction="row">
-        <Title size="h3">Skupiny zaměstanců</Title>
+      {!selectedGroup ? (
+        <>
+          <Flex gap={15} direction="row">
+            <Title size="h3">Skupiny zaměstanců</Title>
 
-        <Button onClick={open} variant="filled">
-          Přidat
-        </Button>
-      </Flex>
+            <Button onClick={open} variant="filled">
+              Přidat
+            </Button>
+          </Flex>
+          <Modal opened={modalOpened} onClose={close} title="Pridat skupinu">
+            <TextInput
+              value={createGroupData.name}
+              onChange={(e) => setCreateGroupData({ ...createGroupData, name: e.target.value })}
+              withAsterisk
+              label="Nazev skupiny"
+              placeholder="Zadej..."
+            />
 
-      <Modal opened={modalOpened} onClose={close} title="Pridat skupinu">
-        <TextInput 
-            value={createGroupData.name} 
-            onChange={(e) => setCreateGroupData({ ...createGroupData, name: e.target.value })}
-            withAsterisk 
-            label="Nazev skupiny" 
-            placeholder="Zadej..." 
-        />
+            <Text mt={10} mb={5} size={"sm"}>
+              Barva
+            </Text>
+            <ColorPicker
+              value={createGroupData.color}
+              onChange={(color) => setCreateGroupData({ ...createGroupData, color })}
+            />
 
-        <Text mt={10} mb={5} size={"sm"}>
-          Barva
-        </Text>
-        <ColorPicker 
-            value={createGroupData.color} 
-            onChange={(color) => setCreateGroupData({ ...createGroupData, color })} 
-        />
+            <Button mt={15} loading={createGroup.isLoading} onClick={() => createGroup.mutateAsync(createGroupData)}>
+              Pridat
+            </Button>
+          </Modal>
 
-        <Button 
-            mt={15}
-            loading={createGroup.isLoading}
-            onClick={() => createGroup.mutateAsync(createGroupData)}
-        >
-            Pridat
-        </Button>
-      </Modal>
+          <Flex direction="row" wrap="wrap" gap={18} mt={20}>
+            {workers.data?.map((el) => {
+              return (
+                <WorkerGroupCard
+                  workerCount={el.workers}
+                  createdAt={el.createdAt}
+                  color={el.color}
+                  category="Nic"
+                  title={el.name}
+                  onClick={() => setSelectedGroup(el)}
+                />
+              );
+            })}
+          </Flex>
+        </>
+      ) : (
+        <>
+          <Flex gap={15} direction="row">
+            <Title size="h3">Skupina {selectedGroup.name}</Title>
 
-      <WorkerGroupCard
-              workerCount={20}
-              createdAt={new Date()}
-              color="green"
-              category="fdklfjd"
-              title="flkdjflksd fjsldk f" onClick={function (): void {
-                  throw new Error("Function not implemented.");
-              } }      />
+            <Button 
+                color="red" 
+                onClick={() => deleteWorkerGroup.mutateAsync(selectedGroup.id)} 
+                variant="outline"
+                loading={deleteWorkerGroup.isLoading}
+            >
+              Smazat
+            </Button>
+          </Flex>
+        </>
+      )}
     </div>
   );
 };
