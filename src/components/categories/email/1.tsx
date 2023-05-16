88a5264b785481eb56a341fc1b557504f1e98704
Matěj Pešl <matejpesl1@gmail.com>
2023-05-17 00:44:14 +0200

    fix emails

--- src/components/categories/email/1.tsx
+++ src/components/categories/email/1.tsx
@@ -3,18 +3,9 @@ import React, {useEffect, useState} from "react";
 
 export default function EmailExercise1() {
 
-    const {data} = api.mail.createPhishingEmail.useQuery(undefined, {
-        refetchOnMount: false,
-        refetchInterval: false,
-        refetchOnReconnect: false,
-        refetchOnWindowFocus: false,
-        refetchIntervalInBackground: false,
-    });
-
     return (
         <div>
-            {data?.subject || "Loading..."}
-            {data?.content || "Loading..."}
+
         </div>
     );
 };
