3d429aa605cb7181a6397d23290d47b98fad062b
Matěj Pešl <matejpesl1@gmail.com>
2023-05-16 14:30:10 +0200

    add /pages/test.tsx to .gitignore

--- src/pages/cock/index.tsx
+++
@@ -1,8 +0,0 @@
-import ExerciseContainer from "../../components/ExerciseContainer";
-import SocialEngineering from "../../components/categories/OnlineKonv/Social-engineering";
-
-export default function Home() {
-    return (
-        <SocialEngineering index={0}/>
-    );
-}
