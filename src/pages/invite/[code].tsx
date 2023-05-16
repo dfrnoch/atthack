53afeca8de293a8340f81c0bd458b3558023902e
lnxcz <frnoch@pm.me>
2023-05-16 22:38:35 +0200

    feat: :sparkles: joe biden

--- src/pages/invite/[code].tsx
+++ src/pages/invite/[code].tsx
@@ -1,26 +1,39 @@
-import { Button, Center, Container, Space, Title } from "@mantine/core";
+import { Button, Center, Container, Skeleton, Space, Title } from "@mantine/core";
 import { signIn, useSession } from "next-auth/react";
 import { api } from "~/utils/api";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
 
 const InvitePage = () => {
   const session = useSession();
   const router = useRouter();
 
   const inviteData = api.invite.getInviteInfo.useQuery(router.query.code as string);
+  const acceptInvite = api.invite.acceptInvite.useMutation();
 
   return (
     <Container className="h-screen">
       <Center className="h-full">
         <div className="flex flex-col items-center gap-6">
-          <Title className="text-2xl">Pozvanka</Title>
+          <Title className="text-2xl">Obdrželi jste pozvánku.</Title>
 
-          {(session.status !== "authenticated" && (
-            <Button variant="filled" onClick={() => signIn()}>
-              Přihlásit se
-            </Button>
-          )) || <div className="w-full">cau p</div>}
+          {(inviteData.status === "loading" && <Skeleton width={100} height={15} />) ||
+            (inviteData.data &&
+              ((session.status !== "authenticated" && (
+                <Button variant="filled" onClick={() => signIn()}>
+                  Přihlásit se
+                </Button>
+              )) || (
+                <div className="w-full">
+                  <Button
+                    variant="filled"
+                    onClick={() => {
+                      acceptInvite.mutateAsync(inviteData.data?.id || "");
+                    }}
+                  >
+                    Přijmout pozvánku
+                  </Button>
+                </div>
+              ))) || <div className="text-center">Neplatná pozvánka!</div>}
         </div>
       </Center>
     </Container>
--- src/server/api/routers/invite.ts
+++ src/server/api/routers/invite.ts
@@ -12,4 +12,31 @@ export const inviteRouter = createTRPCRouter({
 
     return inviteData;
   }),
+
+  acceptInvite: protectedProcedure.input(z.string()).mutation(async ({ input, ctx }) => {
+    const inviteData = await ctx.prisma.invite.findUnique({
+      where: {
+        id: input,
+      },
+    });
+
+    if (!inviteData) {
+      throw new Error("Invite not found");
+    }
+
+    const user = await ctx.prisma.user.update({
+      where: {
+        id: ctx.session.user.id,
+      },
+      data: {
+        Company: {
+          connect: {
+            id: inviteData.companyId,
+          },
+        },
+      },
+    });
+
+    return user;
+  }),
 });
