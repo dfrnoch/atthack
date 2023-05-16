56e79db7e9aa6cec330d8aabdd95ef603a271f00
Atsukoro1 <dornicakkuba@gmail.com>
2023-05-16 18:37:58 +0200

    feat: 🎸 Some changes

--- src/pages/dashboard/admin/pages/workers.tsx
+++ src/pages/dashboard/admin/pages/workers.tsx
@@ -1,20 +1,38 @@
-import { Button, Flex, Title } from "@mantine/core";
+import { Button, Text, ColorPicker, Flex, Modal, TextInput, Title } from "@mantine/core";
 import WorkerGroupCard from "../../../../components/adminDashboard/WorkerGroupCard";
+import { useDisclosure } from "@mantine/hooks";
 
 const WorkersPage = () => {
-    return (
-        <div>
-            <Flex gap={15} direction="row">
-                <Title size="h3">Skupiny zamestancu</Title>
+  const [modalOpened, { open, close }] = useDisclosure();
 
-                <Button variant="filled">
-                    Pridat
-                </Button>
-            </Flex>
+  return (
+    <div>
+      <Flex gap={15} direction="row">
+        <Title size="h3">Skupiny zamestancu</Title>
 
-            <WorkerGroupCard workerCount={20} createdAt={new Date()} color="green" category="fdklfjd" title="flkdjflksd fjsldk f"/>
-        </div>
-    )
+        <Button onClick={open} variant="filled">
+          Pridat
+        </Button>
+      </Flex>
+
+      <Modal opened={modalOpened} onClose={close} title="Pridat skupinu">
+        <TextInput withAsterisk label="Nazev skupiny" placeholder="Zadej..." />
+
+        <Text mt={10} mb={5} size={"sm"}>Barva</Text>
+        <ColorPicker/>
+
+        <Button mt={15}>Pridat</Button>
+      </Modal>
+
+      <WorkerGroupCard
+        workerCount={20}
+        createdAt={new Date()}
+        color="green"
+        category="fdklfjd"
+        title="flkdjflksd fjsldk f"
+      />
+    </div>
+  );
 };
 
 export default WorkersPage;
