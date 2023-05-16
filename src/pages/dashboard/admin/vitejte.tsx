e62286f50c74b077c2ddc8fbeebfef46fd724ef0
lnxcz <frnoch@pm.me>
2023-05-16 20:03:25 +0200

    feat: :sparkles: firma generator

--- src/pages/dashboard/admin/vitejte.tsx
+++ src/pages/dashboard/admin/vitejte.tsx
@@ -1,9 +1,17 @@
 import { Button, Container, TextInput } from "@mantine/core";
+import Router from "next/router";
+import { useEffect, useState } from "react";
+import { api } from "~/utils/api";
 
 const AdminPageWelcome = () => {
+  const createCompany = api.admin.createCompany.useMutation();
+  const [company, setCompany] = useState("");
 
-    
-
+  useEffect(() => {
+    if (createCompany.isSuccess) {
+      Router.replace("/dashboard/admin");
+    }
+  }, [createCompany.isSuccess]);
 
   return (
     <Container>
@@ -11,8 +19,20 @@ const AdminPageWelcome = () => {
       <p>V administraci můžete spravovat všechny části webu, jako jsou články, uživatelé, kategorie, atd.</p>
 
       <div className="mt-5 w-full">
-        <TextInput label="Název firmy" placeholder="Super firma" />
-        <Button type="submit" variant="light" className="mt-3">
+        <TextInput
+          label="Název firmy"
+          placeholder="Super firma"
+          onChange={(event) => setCompany(event.currentTarget.value)}
+        />
+        <Button
+          type="submit"
+          variant="light"
+          className="mt-3"
+          disabled={createCompany.isLoading}
+          onClick={() => {
+            createCompany.mutateAsync(company);
+          }}
+        >
           Vytvořit firmu
         </Button>
       </div>
