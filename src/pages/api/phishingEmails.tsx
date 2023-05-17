c3ff189010aac1f42e3aaf8602a336aa1b317114
Matěj Pešl <matejpesl1@gmail.com>
2023-05-17 10:33:41 +0200

    remove placeholder links in phishing email templates

--- src/pages/api/phishingEmails.tsx
+++ src/pages/api/phishingEmails.tsx
@@ -4,7 +4,7 @@ const phishingEmails = [
         content: `Zdravíme {oslovení} {jméno},
 Vaše verze Windows je zastaralá. Prosím aktualizujte systém na nejnovější verzi.
 Pokud tento email čtete, je nutné ihned aktualizovat systém. V opačném případě může dojít k zablokování vašeho účtu.
-Klikněte na tento odkaz pro aktualizaci: <a href="{target_link}" style="word-wrap: break-word;">{target_link}</a>
+Klikněte na tento odkaz pro aktualizaci: odkaz
 Děkujeme za pochopení.
 IT oddělení`
     },
@@ -12,7 +12,7 @@ IT oddělení`
         subject: "Office 365 - Aktualizace",
         content: `Dobrý den {oslovení} {jméno},
 Je potřeba aktualizovat váš balíček Office 365.
-Váš balíček Office 365 můžete aktualizovat kliknutím na tento odkaz: <a href="{target_link}" style="word-wrap: break-word;">{target_link}</a>
+Váš balíček Office 365 můžete aktualizovat kliknutím na tento odkaz: odkaz
 Děkujeme,
 IT oddělení`
     },
@@ -21,14 +21,14 @@ IT oddělení`
         content: `Dobrý den {oslovení} {jméno},
 Byli nalezeny nějaké neshody ve vašem daňovém přiznání. Prosím otevřete přiložený odkaz a opravte chyby.
 Je ovšem nutné, aby jste se přihlásili do systému přes vaše přihlašovací údaje.
-Přihlášení: <a href="{target_link}" style="word-wrap: break-word;">{target_link}</a>
+Přihlášení: odkaz
 Děkujeme,
 Účetní oddělení`
     },
     {
         subject: "Změna odměn zaměstnancům",
         content: `Dobrý den {oslovení} {jméno},
-Plánované změny odměn zaměstnancům jsou k dispozici na tomto odkazu: <a href="{target_link}" style="word-wrap: break-word;">{target_link}</a>
+Plánované změny odměn zaměstnancům jsou k dispozici na tomto odkazu: odkaz
 Nezapomeňte že je nutné se přihlásit do systému přes vaše přihlašovací údaje.
 Děkujeme,
 Vedení společnosti`
